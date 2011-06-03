887b5e9b40374e56e67952a182a023d83561475c
Ryan Gibbons <insanity5902@gmail.com>
2011-06-02 21:47:24 -0500

    basic php download of image for paulify

---
+++ paulify.php
@@ -0,0 +1,34 @@
+<?php
+
+  $files = array();
+  $path = 'i/s/';
+
+  if ( $dir = opendir( $path ) ) {
+    while ( false !== ( $file = readdir( $dir ) ) ){
+      switch( mime_content_type( $path.$file ) ) {
+        case 'image/jpeg':
+        case 'image/jpg':
+        case 'image/png':
+        case 'image/gif':
+        case 'iamge/bmp':
+          $files[] = $path . $file;
+          break;
+      }
+    }
+  }
+
+  $key = ( array_rand( $files ) );
+
+  header('Content-Description: File Transfer');
+  header('Content-Type: ' . mime_content_type($files[$key]));
+  header('Content-Disposition: attachment; filename='.basename($files[$key]));
+  header('Content-Transfer-Encoding: binary');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+  header('Pragma: public');
+  header('Content-Length: ' . filesize($files[$key]));
+  ob_clean();
+  flush();
+  readfile($files[$key]);
+
+  ?>
